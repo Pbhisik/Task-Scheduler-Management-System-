6e87e67b8a1ea0a5f743968af2377b6a9cc5b94d
Pbhisik <poojabhisikar04@gmail.com>
2024-10-24 07:33:18 +0530

    Intial commit for Task scheduler Project

---
+++ scheduler.py
@@ -0,0 +1,28 @@
+import scheduler
+import time
+from pymongo import MongoClient
+from datetime import datetime
+from bson import ObjectId
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['FlaskDB']
+tasks_collection = db['task1']
+
+def run_scheduled_task():
+    while True:
+        scheduler.run_pending()
+        time.sleep(1)
+
+def execute_task(task_id):
+    task = tasks_collection.find_one({"_id":ObjectId(task_id)})
+    if task:
+        print(f"Executing task: {task['name']}")
+        tasks_collection.update_one({"_id": ObjectId(task_id)})
+
+def scheduler_tasks():
+    tasks = tasks_collection.find()
+    for task in tasks :
+        schedule_time = task ['schedule_time']
+        scheduler.every().day.at(schedule_time).do(execute_task,task_id=task['_id'])
+
+scheduler_tasks()
